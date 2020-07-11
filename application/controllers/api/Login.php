34cffc38641a63439b0273c50894c5769928c13f
Yonathan Rizky Nathanael <jrizky.jonathan@gmail.com>
2020-07-11 20:13:13 +0700

    Refactor API Login

--- application/controllers/api/Login.php
+++ application/controllers/api/Login.php
@@ -22,16 +22,6 @@ class Login extends REST_Controller
         $username = strtolower($this->post('username'));
         $password = md5($this->post('password'));
         $nama = $this->post('nama');
-        $data = [
-            'username' => $this->post('username'),
-            'password' => md5($this->post('password')),
-            'lupddt' => date('Y-m-d'),
-            'lupdtime' => date('H:i:s')
-        ];
-
-        if($this->post('method') == "register"){
-            $this->register($data, $username);
-        }
 
         if(empty($nama)){
             $userLogin = $this->Login_model->userLogin($username);
@@ -40,45 +30,23 @@ class Login extends REST_Controller
                 $payload['iat'] = $this->date_reg->getTimestamp(); 
                 $this->response([
                     'status' => TRUE,
-                    'data' => 'Login Sukses',
-                    'token' => JWT::encode($payload,$this->secretkey),
-                    'fullnm' => $userLogin['fullnm']
+                    'message' => 'Login Sukses',
+                    'data' => [
+                        'token' => JWT::encode($payload,$this->secretkey),
+                        'fullnm' => $userLogin['fullnm'],
+                        'photo' => $userLogin['photo']
+                    ]
                 ], REST_Controller::HTTP_OK);
             }else{
                 $this->response([
                     'status' => FALSE,
-                    'data' => 'Login Gagal, Masukan Email & Password yang terdaftar'
+                    'message' => 'Login Gagal, Masukan Email & Password yang terdaftar'
                 ], REST_Controller::HTTP_OK);
             }
         }
     }
 
-    public function register($data, $username)
-    {
-        $checkUser = $this->Login_model->checkUser($username);
-        
-        if($checkUser){
-            $this->response([
-                'status' => FALSE,
-                'data' => 'User Telah Terdaftar'
-            ], REST_Controller::HTTP_OK);
-        }
-        if ($this->Login_model->addUser($data) > 0) {
-            $payload['username'] = $username;
-            $payload['iat'] = $this->date_reg->getTimestamp(); 
-            $this->response([
-                'status' => TRUE,
-                'data' => 'Berhasil Melakukan Registrasi',
-                'token' => JWT::encode($payload,$this->secretkey)
-            ], REST_Controller::HTTP_OK);
-        }else{
-            $this->response([
-                'status' => FALSE,
-                'data' => 'Gagal Melakukan Registrasi'
-            ], REST_Controller::HTTP_OK);
-        }
-    }
-
+    
     public function cektoken(){
         $jwt = $this->input->get_request_header('Authorization');
         try {
@@ -89,7 +57,7 @@ class Login extends REST_Controller
         } catch (Exception $e) {
             $this->response([
                 'status' => FALSE,
-                'data' => 'Wrong Token'
+                'message' => 'Wrong Token'
             ], REST_Controller::HTTP_OK);
         }
     }
