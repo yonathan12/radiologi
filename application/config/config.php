bcaf2519a3893f8da53f5a8edce4de09ecefc30f
Yonathan Rizky Nathanael <jrizky.jonathan@gmail.com>
2020-08-04 18:31:02 +0700

    Update API Dosis

--- application/config/config.php
+++ application/config/config.php
@@ -23,10 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-## local
-// $config['base_url'] = 'http://localhost/bethesda/';
-## online
-$config['base_url'] = 'https://radiologi.yonathanrizky.com/';
+$config['base_url'] = 'http://localhost/bethesda/';
 
 /*
 |--------------------------------------------------------------------------
